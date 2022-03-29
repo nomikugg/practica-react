0bd02353b642c721fd965a28a250852a12904801
Fernando <fernandoarroyo0011@gmail.com>
2022-03-29 01:13:44 -0400

    Agregando scripts de Clases

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Propiedades from "./components/Propiedades";
 import Estado from "./components/Estado";
 import RenderizadoCondicional from "./components/RenderizadoCondicional";
 import RenderizadoElementos from "./components/RenderizadoElementos";
-import { EventosES6, EventosES7 } from "./components/Eventos";
+import { EventosES6, EventosES7, MasSobreEventos } from "./components/Eventos";
 
 function App() {
   return (
@@ -60,6 +60,10 @@ function App() {
           <EventosES6></EventosES6>
           <hr />
           <EventosES7></EventosES7>
+
+          <hr />
+
+          <MasSobreEventos></MasSobreEventos>
         </section>
       </header>
     </div>
--- src/components/Eventos.js
+++ src/components/Eventos.js
@@ -76,3 +76,66 @@ export class EventosES7 extends Component {
     );
   }
 }
+
+// function Boton(props) {
+//   return <button onClick={props.myOnClick}> Boton hecho componente</button>;
+// }
+
+// const Boton = props => (
+//   <button onClick={props.myOnClick}> Boton hecho componente</button>
+// );
+
+const Boton = ({ myOnClick }) => (
+  <button onClick={myOnClick}> Boton hecho componente</button>
+);
+
+// Tres maneras de hacer este componente
+
+export class MasSobreEventos extends Component {
+  handleClick = (e, mensaje) => {
+    console.log(e);
+    console.log(e.nativeEvent);
+    console.log(e.target);
+    console.log(e.nativeEvent.target);
+    console.log(mensaje);
+  };
+  render() {
+    return (
+      <div>
+        <h2>Mas Sobre Eventos</h2>
+        <button
+          onClick={e =>
+            this.handleClick(
+              e,
+              "Hola, Pasando mensaje desde un parametro, arrow function"
+            )
+          }
+        >
+          Saludar
+        </button>
+        {/* //El evento onClick es un evento de JSX,
+        //Pero no asi de un componente
+        Personalizado
+        //Evento personalizado
+        */}
+        {/* <Boton
+          onClick={e =>
+            this.handleClick(
+              e,
+              "Hola, Pasando mensaje desde un parametro, arrow function"
+            )
+          }
+        ></Boton> */}
+
+        <Boton
+          myOnClick={e =>
+            this.handleClick(
+              e,
+              "Hola, Pasando mensaje desde un parametro, arrow function"
+            )
+          }
+        ></Boton>
+      </div>
+    );
+  }
+}
