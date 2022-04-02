4ab27a85e02c8237da7767f770270f085370dbba
Fernando <fernandoarroyo0011@gmail.com>
2022-04-01 23:50:34 -0400

    Agregando Ajax API pokemon

--- src/components/AjaxApis.js
+++ src/components/AjaxApis.js
@@ -1,6 +1,24 @@
 import React, { Component } from "react";
 
+function Pokemon(props) {
+  return (
+    <figure>
+      <img src={props.avatar} alt={props.name} />
+      <figcaption>{props.name}</figcaption>
+    </figure>
+  );
+}
 export default class AjaxApis extends Component {
+  state = {
+    pokemon: [],
+  };
+
+  componentDidMount() {
+    let url = "https://pokeapi.co/api/v2/pokemon/";
+    fetch(url)
+      .then(res => res.json())
+      .then(json => console.log(json));
+  }
   render() {
     return (
       <>
