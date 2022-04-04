4e02805a688a087cf19bde692ba7fd688f1a7e56
Fernando <fernandoarroyo0011@gmail.com>
2022-04-03 23:27:53 -0400

    Agregando Hooks, funciones

--- src/components/AjaxApis.js
+++ src/components/AjaxApis.js
@@ -10,19 +10,43 @@ function Pokemon(props) {
 }
 export default class AjaxApis extends Component {
   state = {
-    pokemon: [],
+    pokemons: [],
   };
 
   componentDidMount() {
     let url = "https://pokeapi.co/api/v2/pokemon/";
     fetch(url)
       .then(res => res.json())
-      .then(json => console.log(json));
+      .then(json => {
+        //console.log(json);
+        // console.log(json.results);
+        json.results.forEach(element => {
+          fetch(element.url)
+            .then(dataPoke => dataPoke.json())
+            .then(dataJson => {
+              console.log(dataJson);
+              let pokemon = {
+                id: dataJson.id,
+                name: dataJson.name,
+                img: dataJson.sprites.front_default,
+              };
+              let pokemons = [...this.state.pokemons, pokemon];
+              this.setState({ pokemons });
+            });
+        });
+      });
   }
   render() {
     return (
       <>
         <h2>Peticiones asincronas en Componentes de clase</h2>
+        {this.state.pokemons.length === 0 ? (
+          <h3>Cargando</h3>
+        ) : (
+          this.state.pokemons.map(el => (
+            <Pokemon key={el.id} name={el.name} avatar={el.img} />
+          ))
+        )}
       </>
     );
   }
